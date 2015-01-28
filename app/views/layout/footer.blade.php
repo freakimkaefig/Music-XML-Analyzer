6432acb4048a57ed997f2d09653693ff851aa439
freakimkaefig <lukas.lamm89@googlemail.com>
2015-01-28 13:35:55 +0100

    Added twitter bootstrap library
    Source twitter bootstrap, for compiling from source check http://getbootstrap.com/getting-started/#grunt or ask me

---
+++ app/views/layout/footer.blade.php
@@ -0,0 +1,7 @@
+<nav id="footer-nav" class="navbar navbar-inverse navbar-fixed-bottom" role="navigation">
+	<div class="container-fluid">
+		<ul class="nav navbar-nav navbar-left">
+      		<li><a href="#">&copy; 2015 Music XML Analyzer</a></li>
+  		</ul>
+	</div>
+</nav>
---
+++ app/views/layout/scripts.blade.php
@@ -0,0 +1,2 @@
+{{ HTML::script('libs/jquery-1.11.2/jquery-1.11.2.min.js') }}
+{{ HTML::script('libs/bootstrap-3.3.2/dist/js/bootstrap.min.js') }}
