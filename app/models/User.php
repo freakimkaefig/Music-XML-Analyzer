b1b0fd14309e5bacf9db5559ead36c1118406127
freakimkaefig <lukas.lamm89@googlemail.com>
2015-01-29 18:51:04 +0100

    Created model classes for database tables (renamed File -> Upload)

--- app/models/User.php
+++ app/models/User.php
@@ -1,26 +1,15 @@
 <?php
 
-use Illuminate\Auth\UserTrait;
-use Illuminate\Auth\UserInterface;
-use Illuminate\Auth\Reminders\RemindableTrait;
-use Illuminate\Auth\Reminders\RemindableInterface;
-
-class User extends Eloquent implements UserInterface, RemindableInterface {
-
-	use UserTrait, RemindableTrait;
-
-	/**
-	 * The database table used by the model.
-	 *
-	 * @var string
-	 */
+class User extends Eloquent {
+	
 	protected $table = 'users';
 
-	/**
-	 * The attributes excluded from the model's JSON form.
-	 *
-	 * @var array
-	 */
-	protected $hidden = array('password', 'remember_token');
+	protected $softDelete = true;
+
+	protected $fillable = array('last_activity');
+
+	public function files() {
+		return $this->hasMany('Upload');
+	}
 
 }
