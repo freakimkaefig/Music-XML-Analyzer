c22b83da9b6ff9ca5c4877335fb3e6d6f1577d42
semmler1988 <semmel-tobi@gmx.de>
2015-02-03 15:31:24 +0100

    various adjustments

--- app/views/dashboard.blade.php
+++ app/views/dashboard.blade.php
@@ -6,15 +6,19 @@
 	<div class="col-xs-12">
 		<h1><center>Welcome to the Dashboard</h1>
 	
-		<div id="pie1" class="col-xs-4"></div>
-		<div id="pie2" class="col-xs-4"></div>
-		<div id="pie3" class="col-xs-4"></div>
+		<div id="pie1" class="pie col-xs-4"></div>
+		<div id="pie2" class="pie col-xs-4"></div>
+		<div id="pie3" class="pie col-xs-4"></div>
 
 
 		<div class="chart col-xs-6">
 			<h4>Different Statistics</h4>
 		</div>
+
+
 	</div>
+
+	<div class="col-xs-6 "id="chart"></div> 
 </div>
 
 @stop
--- public/js/views/DashboardView.js
+++ public/js/views/DashboardView.js
@@ -6,7 +6,7 @@ var that = {},
 	init = function(){
 		console.info('MusicXMLAnalyzer.DashboardView.init');
 
-//PIE-CHART
+	//PIE-CHARTS
 	var pie = new d3pie("pie1", {
 		header: {
 			title: {
@@ -15,15 +15,27 @@ var that = {},
 		},
 		data: {
 			content: [
-				{ label: "C", value: 264131 },
-				{ label: "D", value: 218812 },
-				{ label: "E", value: 157618 },
-				{ label: "F", value: 264131 },
-				{ label: "G", value: 518812 },
-				{ label: "A", value: 265131 },
-				{ label: "H", value: 111112 }
+				{ label: "C", value: 2 },
+				{ label: "D", value: 21 },
+				{ label: "E", value: 15 },
+				{ label: "F", value: 26 },
+				{ label: "G", value: 51 },
+				{ label: "A", value: 20 },
+				{ label: "H", value: 11 }
 			]
 		},
+		tooltips: {
+		    enabled: true,
+		    type: "placeholder",
+		    string: "{label}: ({value})  {percentage}%",
+		    // data is an object with the three properties listed below. Just modify the properties
+		    // directly - there's no need to return anything
+		    placeholderParser: function(index, data) {
+		      data.label = data.label + "  ";
+		      data.percentage = data.percentage;
+		      data.value = data.value;
+		    }
+		  }
 	});  
 
 	pie = new d3pie("pie2", {
@@ -34,11 +46,11 @@ var that = {},
 		},
 		data: {
 			content: [
-				{ label: "1", value: 264131 },
-				{ label: "1/2", value: 218812 },
-				{ label: "1/4", value: 157618 },
-				{ label: "3/4", value: 264131 },
-				{ label: "5/8", value: 518812 }
+				{ label: "1", value: 26 },
+				{ label: "1/2", value: 21 },
+				{ label: "1/4", value: 15 },
+				{ label: "3/4", value: 26 },
+				{ label: "5/8", value: 51 }
 			]
 		},
 	}); 
@@ -51,36 +63,37 @@ var that = {},
 		},
 		data: {
 			content: [
-				{ label: "Violine", value: 264131 },
-				{ label: "Gitarre", value: 218812 },
-				{ label: "Bass", value: 157618 },
-				{ label: "Klarinette", value: 264131 },
-				{ label: "Schlagzeug", value: 518812 },
-				{ label: "Horn", value: 518812 },
-				{ label: "Flöte", value: 518812 }
+				{ label: "Violine", value: 11 },
+				{ label: "Gitarre", value: 2 },
+				{ label: "Bass", value: 13 },
+				{ label: "Klarinette", value: 26 },
+				{ label: "Schlagzeug", value: 51 },
+				{ label: "Horn", value: 39 },
+				{ label: "Flöte", value: 5 }
 			]
 		},
+		 tooltips: {
+		    enabled: true,
+		    type: "placeholder",
+		    string: "{label}: {percentage}%",
+		    styles: {
+		      fadeInSpeed: 500,
+		      backgroundColor: "#00cc99",
+		      backgroundOpacity: 0.8,
+		      color: "#ffffcc",
+		      borderRadius: 4,
+		      font: "verdana",
+		      fontSize: 20,
+		      padding: 20
+		    }
+		  }
 	}); 
 
 
-	/*//BAR-CHART 1
-	data = [8,46,57,24,21,24,26];
+	//BAR-CHART1
 
-	 x = d3.scale.linear()
-	    .domain([0, d3.max(data)])
-	    .range([0, 300]);
-
-	d3.select(".chart")
-	  .selectAll("div")
-	    .data(data)
-	  .enter().append("div")
-	    .style("width", function(d) { return x(d) + "px" })
-	    .text(function(d) { return d });  */
-
-	//BAR-CHART 2
-
-	var names = ['John', 'Tim', 'Sam', 'Greg', 'Charles'],
-	    hotdogs = [8, 15, 20, 12, 25],
+	var names = ['Anzahl Takte', 'Anzahl Noten', 'Anzahl Instrumente'],
+	    occ_numbers = [34, 15, 20],
 	    chart,
 	    width = 500,
 	    bar_height = 20,
@@ -88,20 +101,20 @@ var that = {},
 
 	var x, y;
 	x = d3.scale.linear()
-	   .domain([0, d3.max(hotdogs)])
+	   .domain([0, d3.max(occ_numbers)])
 	   .range([0, width]);
 
 	y = d3.scale.ordinal()
-	   .domain(hotdogs)
+	   .domain(occ_numbers)
 	   .rangeBands([0, height]);
 
 
-	var left_width = 100;
+	var left_width = 150;
 
 	var gap = 5;
  	// redefine y for adjusting the gap
   	y = d3.scale.ordinal()
-    	.domain(hotdogs)
+    	.domain(occ_numbers)
    		.rangeBands([0, (bar_height + 2 * gap) * names.length]);
  
  
@@ -114,7 +127,7 @@ var that = {},
 	    .attr("transform", "translate(10, 20)");
 
 	chart.selectAll("line")
-		.data(x.ticks(d3.max(hotdogs)))
+		.data(x.ticks(10))		//d3.max(occ_numbers) zeigts in 1er schritten
 		.enter().append("line")
 		.attr("x1", function(d) { return x(d) + left_width; })
 		.attr("x2", function(d) { return x(d) + left_width; })
@@ -122,7 +135,7 @@ var that = {},
 		.attr("y2", (bar_height + gap * 2) * names.length);
  
 	chart.selectAll(".rule")
-		.data(x.ticks(d3.max(hotdogs)))
+		.data(x.ticks(10))		//d3.max(occ_numbers) zeigts in 1er schritten
 		.enter().append("text")
 		.attr("class", "rule")
 		.attr("x", function(d) { return x(d) + left_width; })
@@ -133,7 +146,7 @@ var that = {},
 		.text(String);
 
 	chart.selectAll("rect")
-		.data(hotdogs)
+		.data(occ_numbers)
 		.enter().append("rect")
 		.attr("x", left_width)
 		.attr("y", y)
@@ -141,7 +154,7 @@ var that = {},
 		.attr("height", y.rangeBand());
 
 	chart.selectAll("text.score")
-		.data(hotdogs)
+		.data(occ_numbers)
 		.enter().append("text")
 		.attr("x", function(d) { return x(d) + left_width; })
 		.attr("y", function(d){ return y(d) + y.rangeBand()/2; } )
@@ -155,12 +168,88 @@ var that = {},
 		.data(names)
 		.enter().append("text")
 		.attr("x", left_width / 2)
-		.attr("y", function(d, i){ return y(hotdogs[i]) + y.rangeBand()/2; } )
+		.attr("y", function(d, i){ return y(occ_numbers[i]) + y.rangeBand()/2; } )
 		.attr("dy", ".36em")
 		.attr("text-anchor", "middle")
 		.attr('class', 'name')
 		.text(String);
 
+
+//BAR-CHART 2
+
+	var margin ={top:20, right:30, bottom:30, left:40},
+	    width=960-margin.left - margin.right, 
+	    height=500-margin.top-margin.bottom;
+
+	// scale to ordinal because x axis is not numerical
+	var x = d3.scale.ordinal().rangeRoundBands([0, width], .1);
+
+	//scale to numerical value by height
+	var y = d3.scale.linear().range([height, 0]);
+
+	var chart = d3.select("#chart")  
+	              .append("svg")  //append svg element inside #chart
+	              .attr("width", width+(2*margin.left)+margin.right)    //set width
+	              .attr("height", height+margin.top+margin.bottom);  //set height
+	var xAxis = d3.svg.axis()
+	              .scale(x)
+	              .orient("bottom");  //orient bottom because x-axis will appear below the bars
+
+	var yAxis = d3.svg.axis()
+	              .scale(y)
+	              .orient("left");
+
+	d3.json("http://codepen.io/superpikar/pen/kcJDf.js", function(error, data){ // link zu testdateie mit 8 key-value paaren:   https://dl.dropboxusercontent.com/u/19535961/example-json.js
+	  x.domain(data.map(function(d){ return d.letter}));
+	  y.domain([0, d3.max(data, function(d){return d.frequency})]);
+	  
+	  var bar = chart.selectAll("g")
+	                    .data(data)
+	                  .enter()
+	                    .append("g")
+	                    .attr("transform", function(d, i){
+	                      return "translate("+x(d.letter)+", 0)";
+	                    });
+	  
+	  bar.append("rect")
+	      .attr("y", function(d) { 
+	        return y(d.frequency); 
+	      })
+	      .attr("x", function(d,i){
+	        return x.rangeBand()+(margin.left/4);
+	      })
+	      .attr("height", function(d) { 
+	        return height - y(d.frequency); 
+	      })
+	      .attr("width", x.rangeBand());  //set width base on range on ordinal data
+
+	  bar.append("text")
+	      .attr("x", x.rangeBand()+margin.left )
+	      .attr("y", function(d) { return y(d.frequency) -10; })
+	      .attr("dy", ".75em")
+	      .text(function(d) { return d.frequency; });
+	  
+	  chart.append("g")
+	        .attr("class", "x axis")
+	        .attr("transform", "translate("+margin.left+","+ height+")")        
+	        .call(xAxis);
+	  
+	  chart.append("g")
+	        .attr("class", "y axis")
+	        .attr("transform", "translate("+margin.left+",0)")
+	        .call(yAxis)
+	        .append("text")
+	        .attr("transform", "rotate(-90)")
+	        .attr("y", 6)
+	        .attr("dy", ".71em")
+	        .style("text-anchor", "end")
+	        .text("Frequency");
+	});
+
+	function type(d) {
+	    d.letter = +d.letter; // coerce to number
+	    return d;
+	  }
 
 
 	};
