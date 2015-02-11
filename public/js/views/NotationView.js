a73761924f85e70eefb0b4e4c627d7235559a9dc
matTailor <Matthias.Schneider@stud.uni-regensburg.de>
2015-02-11 17:29:17 +0100

    mouse over canvas logs corresponding note

--- public/js/views/NotationView.js
+++ public/js/views/NotationView.js
@@ -8,6 +8,9 @@ MusicXMLAnalyzer.NotationView = function(){
 	canvasLeft = null,
 	canvasTop = null,
 
+	paddingTopStaves = 0;
+	spaceBetweenLines = 0;
+
 	init = function() {
 		console.log("notation view");
 		initCanvas();
@@ -30,7 +33,9 @@ MusicXMLAnalyzer.NotationView = function(){
 	/* This method adds the 5 note lines to canvas */
 	addStaveElements = function() {
 
-		var spaceBetweenLines = (canvas.height/14) * 5;
+		paddingTopStaves = (canvas.height/14) * 5;
+		spaceBetweenLines = (canvas.height/14);
+		
 		console.log("c h: " + spaceBetweenLines);
 
 		for(var i = 0; i < 5; i++) {
@@ -39,20 +44,80 @@ MusicXMLAnalyzer.NotationView = function(){
 			    colour: '#000000',
 			    width: canvas.width,
 			    height: 1.5,
-			    top: spaceBetweenLines + (12 * i),
+			    top: paddingTopStaves + (spaceBetweenLines * i),
 			    left: 0
 			});	
 		}
 		
 	},
 
+	/* This method handels the mouseover event of canvas */
 	onMouseMoveCanvas = function(event) {
-		console.log("mouse over canvas");
+
 		var x = event.pageX - canvasLeft,
-		        y = event.pageY - canvasTop;
-		    	console.log(x, y);
-		//TODO
-		//hier die erkennung für notenlinien abfragen
+	        y = event.pageY - canvasTop;
+
+    	// console.log("y: " + y);
+
+    	checkHorizontalArea(y);
+
+	//TODO
+	//hier die erkennung für notenlinien abfragen
+	},
+
+	checkHorizontalArea = function(y) {
+
+		var horizontalArea = -1;
+
+		if (y > spaceBetweenLines * 1.25 && y <= spaceBetweenLines * 1.75) {
+			console.log("f");
+		} else if (y > spaceBetweenLines * 1.75 && y <= spaceBetweenLines * 2.25) {
+			console.log("e");
+		} else if (y > spaceBetweenLines * 2.25 && y <= spaceBetweenLines * 2.75) {
+			console.log("d");
+		} else if (y > spaceBetweenLines * 2.75 && y <= spaceBetweenLines * 3.25) {
+			console.log("c");
+		} else if (y > spaceBetweenLines * 3.25 && y <= spaceBetweenLines * 3.75) {
+			console.log("h");
+		} else if (y > spaceBetweenLines * 3.75 && y <= spaceBetweenLines * 4.25) {
+			console.log("a");
+		} else if (y > spaceBetweenLines * 4.25 && y <= spaceBetweenLines * 4.75) {
+			console.log("g");
+		} else if (y > spaceBetweenLines * 4.75 && y <= spaceBetweenLines * 5.25) {
+			console.log("f");
+		} else if (y > spaceBetweenLines * 5.25 && y <= spaceBetweenLines * 5.75) {
+			console.log("e");
+		} else if (y > spaceBetweenLines * 5.75 && y <= spaceBetweenLines * 6.25) {
+			console.log("d");
+		} else if (y > spaceBetweenLines * 6.25 && y <= spaceBetweenLines * 6.75) {
+			console.log("c");
+		} else if (y > spaceBetweenLines * 6.75 && y <= spaceBetweenLines * 7.25) {
+			console.log("h");
+		} else if (y > spaceBetweenLines * 7.25 && y <= spaceBetweenLines * 7.75) {
+			console.log("a");
+		} else if (y > spaceBetweenLines * 7.75 && y <= spaceBetweenLines * 8.25) {
+			console.log("g");
+		} else if (y > spaceBetweenLines * 8.25 && y <= spaceBetweenLines * 8.75) {
+			console.log("f");
+		} else if (y > spaceBetweenLines * 8.75 && y <= spaceBetweenLines * 9.25) {
+			console.log("e");
+		} else if (y > spaceBetweenLines * 9.25 && y <= spaceBetweenLines * 9.75) {
+			console.log("d");
+		} else if (y > spaceBetweenLines * 9.75 && y <= spaceBetweenLines * 10.25) {
+			console.log("c");
+		} else if (y > spaceBetweenLines * 10.25 && y <= spaceBetweenLines * 10.75) {
+			console.log("h");
+		} else if (y > spaceBetweenLines * 10.75 && y <= spaceBetweenLines * 11.25) {
+			console.log("a");
+		} else if (y > spaceBetweenLines * 11.25 && y <= spaceBetweenLines * 11.75) {
+			console.log("g");
+		} else if (y > spaceBetweenLines * 11.75 && y <= spaceBetweenLines * 12.25) {
+			console.log("f");
+		} else if (y > spaceBetweenLines * 12.25 && y <= spaceBetweenLines * 12.75) {
+			console.log("e");
+		}
+		// return horizontalArea;
+
 	},
 
 	addOnStaveClickListener = function() {
