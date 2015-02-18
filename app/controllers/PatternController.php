a0689ef29817036af90d772a0406604beb937f8f
freakimkaefig <lukas.lamm89@googlemail.com>
2015-02-18 11:49:24 +0100

    Updated basic structure for pattern search logic

--- app/controllers/PatternController.php
+++ app/controllers/PatternController.php
@@ -3,20 +3,23 @@ require 'SoundSequenzController.php';
 
 class PatternController extends BaseController {
 	public function getCreatePattern() {
+		return View::make('createPattern');
+	}
+
+	public function postPatternSearch() {
 		// pattern = pattern
 		// type = tonfolge ? thythmus ? melodie?
 
 		//if type = tonfolge
 		$ssConntroller = new SoundSequenzController();
+		$ssConntroller->search();
 
 		// elseif type = rhythmus
-		$rConntroller = new RhythmController();
+		// $rConntroller = new RhythmController();
 
 		// elseif type = melodie
-		$mConntroller = new MelodyController();
-
-
-		return View::make('createPattern');	
+		// $mConntroller = new MelodyController();
+		
 	}
 
 }
--- app/controllers/SoundSequenzController.php
+++ app/controllers/SoundSequenzController.php
@@ -7,4 +7,16 @@ class SoundSequenzController {
 		echo "<br><br><br>CREATED NEW SoundSequenzController!";
 	}
 
+	public function search() {
+		$pattern = array(
+			0 => (object)array("step" => 'A', 'accidental' => 'sharp', 'octave' => 2)
+		);
+
+		$results = array(
+			0 => (object)array("Test" => true)
+		);
+
+		return Redirect::route('search_results', array('pattern' => $pattern, 'results' => $results));
+	}
+
 }
