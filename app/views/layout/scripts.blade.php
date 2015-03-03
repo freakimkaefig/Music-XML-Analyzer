eb45000221d0ba52e3fe9cdf59c51e1577ee758a
freakimkaefig <lukas.lamm89@googlemail.com>
2015-03-03 22:00:30 +0100

    Added bootstrap material design
    Documentation:
    https://github.com/FezVrasta/bootstrap-material-design
    http://fezvrasta.github.io/bootstrap-material-design

--- app/views/layout/scripts.blade.php
+++ app/views/layout/scripts.blade.php
@@ -1,5 +1,6 @@
 {{ HTML::script('libs/jquery-1.11.2/jquery-1.11.2.min.js') }}
 {{ HTML::script('libs/bootstrap-3.3.2/dist/js/bootstrap.min.js') }}
+{{ HTML::script('libs/bootstrap-material-design/dist/js/material.min.js') }}
 
 <!-- D3.js -->
 {{ HTML::script('libs/d3-v3/d3.min.js') }}
