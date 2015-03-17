a6e6b4198f4ffb1ed7d90079403e01dc6712128a
led13714 <david.lechler@stud.uni-regensburg.de>
2015-03-17 12:56:51 +0100

    fixxed patterns

--- public/js/views/PatternView.js
+++ public/js/views/PatternView.js
@@ -67,8 +67,7 @@ MusicXMLAnalyzer.PatternView = function(){
 						ptich: {
 							type: "half",
 							dot: true,
-							beam: false,
-							timeModification: false
+							beam: false
 						}
 					},
 					{
@@ -93,12 +92,7 @@ MusicXMLAnalyzer.PatternView = function(){
 							alter: -1,
 							octave: 5,
 							dot: false,
-							beam: "begin",	// Triole (1)
-							timeModification: {
-								actualNotes: 3,
-								normalNotes: 2,
-								normalType: "eight"
-							}
+							beam: "begin"	// Triole (1)
 						}
 					},
 					{
@@ -109,12 +103,7 @@ MusicXMLAnalyzer.PatternView = function(){
 							alter: 0,
 							octave: 5,
 							dot: false,
-							beam: "continue",	// Triole (2)
-							timeModification: {
-								actualNotes: 3,
-								normalNotes: 2,
-								normalType: "eight"
-							}
+							beam: "continue"	// Triole (2)
 						}
 					},
 					{
@@ -125,12 +114,7 @@ MusicXMLAnalyzer.PatternView = function(){
 							alter: 0,
 							octave: 5,
 							dot: false,
-							beam: "end",	// Triole (3)
-							timeModification: {
-								actualNotes: 3,
-								normalNotes: 2,
-								normalType: "eight"
-							}
+							beam: "end"	// Triole (3)
 						}
 					},
 					{
@@ -141,8 +125,7 @@ MusicXMLAnalyzer.PatternView = function(){
 							alter: 0,
 							octave: 5,
 							dot: true,	// punktierte Note
-							beam: false,
-							timeModification: false,
+							beam: false
 						}
 					}
 					{
