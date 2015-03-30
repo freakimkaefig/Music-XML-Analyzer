eaffea1074e7e27ca90968dd5b7a1f6918e007f1
Lukas Lamm <lukas.lamm89@gmail.com>
2015-03-30 22:52:56 +0200

    Updated post build hook

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
 web: vendor/bin/heroku-php-apache2 public/
-worker: sh prebuild.sh
--- prebuild.sh
+++
@@ -1,8 +0,0 @@
-# reset heroku db
-heroku pg:reset HEROKU_POSTGRESQL_CRIMSON --confirm music-xml-analyzer
-
-# migrate db for laravel
-php artisan migrate
-
-# update db for cron
-php artisan migrate --package="liebig/cron"
