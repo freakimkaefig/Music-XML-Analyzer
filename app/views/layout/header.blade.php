c910e2cc4371339d50af52f738aeabc3c7b5b719
led13714 <david.lechler@stud.uni-regensburg.de>
2015-04-07 12:30:09 +0200

    Moved Page title to header

--- app/views/layout/header.blade.php
+++ app/views/layout/header.blade.php
@@ -1,14 +1,38 @@
 <nav class="navbar navbar-material-blue-grey-800 navbar-fixed-top">
 
-    <div class="navbar-header">
-        <a class="navbar-brand" href="{{ URL::route('home') }}">Music XML Analyzer</a>
-    </div>
-	<div class="container-fluid">
-
+    <div class="container-fluid">
+        <div class="col-xs-5 navbar-header">
+            <a class="navbar-brand" href="{{ URL::route('home') }}">Music XML Analyzer</a>
+        </div>
+        <div class="col-xs-2 navbar-brand" id="navbarTitleText">
+            <?php  
+                // Debugbar::info(Route::currentRouteName());
+                $route = Route::currentRouteName();
+                switch($route){
+                    case 'pattern':
+                        echo 'Search';
+                        break;
+                    case 'dashboard':
+                        echo 'Dashboard';
+                        break;
+                    case 'results':
+                        echo 'Search Results';
+                        break;
+                    case 'download':
+                        echo 'Download';
+                        break;
+                    case 'resultDetail':
+                        echo 'Result Details';
+                        break;
+                    default:
+                        break;
+                }
+            ?>
+        </div>
      	<?php if (Cookie::get('user_id')): ?>
      		<?php $user = User::find(Cookie::get('user_id')); ?>
      		<?php if ($user): ?>
-		     	<ul class="nav navbar-nav navbar-right">
+		     	<ul class="col-xs-4 nav navbar-nav navbar-right">
                 <?php $uploads = $user->uploads; ?>
                 <?php if (!$uploads->isEmpty()): ?>
 			     	<li class="btn-flat btn-material-grey-200">
--- app/views/results/list.blade.php
+++ app/views/results/list.blade.php
@@ -8,8 +8,7 @@
 <div class="container">
 	<div class="row">
 		<div class="col-xs-12">
-			<h1 class="text-center">Search results</h1>
-			<h4 class="text-center">for your pattern</h4>
+			<h4 class="text-center">Search resultsfor your pattern</h4>
 			<canvas id="patternCanvas" width="700" height="180"></canvas>
 		</div>
 	</div>
