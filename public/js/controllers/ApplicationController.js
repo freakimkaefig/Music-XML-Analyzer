52a2fc553ee874779726037336d8e7cc2fd00b87
semmler1988 <semmel-tobi@gmx.de>
2015-04-08 14:31:45 +0200

    deleted console.log / console.info usw.

--- public/js/controllers/ApplicationController.js
+++ public/js/controllers/ApplicationController.js
@@ -1,41 +1,29 @@
 MusicXMLAnalyzer.ApplicationController = function() {
 
 	var that = {},
-
 	headerController = null,
 	uploadController = null,
 	dashboardController = null,
 	patternController = null,
 	resultController = null,
 
-
 	init = function() {
-		console.info('MusicXMLAnalyzer.ApplicationController.init');
-
 		headerController = MusicXMLAnalyzer.HeaderController();
 		headerController.init();
-
 		uploadController = MusicXMLAnalyzer.UploadController();
 		uploadController.init();
 
-		if (Route.check('/imprint')) {
-			console.info("Detected route '/imprint'");
-		}
-
 		if (Route.check('/dashboard')) {
-			console.info("Detected route '/dashboard'");
 			dashboardController = MusicXMLAnalyzer.DashboardController();
 			dashboardController.init();
 		}
 
 		if (Route.check('/pattern')) {
-			console.info("Detected route '/pattern'");
 			patternController = MusicXMLAnalyzer.PatternController();
 			patternController.init();
 		}
 
 		if (Route.check('/results')) {
-			console.info("Detected route '/results'");
 			resultController = MusicXMLAnalyzer.ResultController();
 			resultController.init();
 		}
@@ -49,5 +37,4 @@ MusicXMLAnalyzer.ApplicationController = function() {
 	that.dispose = dispose;
 
 	return that;
-
 };
